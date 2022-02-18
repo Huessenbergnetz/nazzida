ff71ad01be493f99fe895740838ce3ad71975dff
Buschmann <mf@huessenbergnetz.de>
2022-02-18 09:17:24 +0100

    [General] add missing license header

--- common/src/migrations/m20220130t123658_liquid.cpp
+++ common/src/migrations/m20220130t123658_liquid.cpp
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: (C) 2022 Matthias Fehring / www.huessenbergnetz.de
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 #include "m20220130t123658_liquid.h"
 
 M20220130T123658_Liquid::M20220130T123658_Liquid(Firfuorida::Migrator *parent) :
